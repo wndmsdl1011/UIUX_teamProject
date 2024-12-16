095d777a2702221902deace21f3c48a241f84942
Jueun Kim <jueun7547@naver.com>
2024-12-16 19:08:41 +0900

    수정

--- Calendar.js
+++ Calendar.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const calendarEl = document.getElementById("calendar");
   const mapSection = document.getElementById("map-section");
   const calendarSection = document.getElementById("calendar-section");
+  const logo = document.querySelector(".logo"); // 로고 요소 선택
 
   const rssBaseUrl = "https://api.rss2json.com/v1/api.json?rss_url=";
   const rssFeeds = {
@@ -60,6 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
   window.showCalendarSection = function (section) {
     mapSection.style.display = "none";    //지도 숨김
     calendarSection.style.display = "block"; //캘린더 표시
+    logo.classList.add("small-logo"); // 로고 크기 줄이기 클래스 추가
 
     setTimeout(() => {
       calendar.updateSize();    //캘린더 크기 업데이트
@@ -70,6 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
   window.showMapSection = function () {
     calendarSection.style.display = "none"; //캘린더 숨김
     mapSection.style.display = "block"; //지도 표시
+    logo.classList.remove("small-logo"); // 로고 크기 줄이기 클래스 제거
   };
 
   // 초기 상태에서는 지도만 표시
